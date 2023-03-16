1abcf3db18c41d0f33c1f9f6279462b0a3ffe370
Ben <benjaminkhlau@gmail.com>
2023-03-15 21:37:58 -0500

    implemented lotto system so members can gamble earned coins

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def balance(ctx):
     
     wallet_amount = users[str(user.id)]["wallet"]
     
-    em = discord.Embed(title = f"{ctx.author.name}'s  balance")
+    em = discord.Embed(title = f"{ctx.author.name}'s  balance", color=discord.Color.blue())
     em.add_field(name = "Wallet balance", value = wallet_amount)
     # print(em, "HELLO")
     await ctx.send(embed = em)
@@ -47,9 +47,9 @@ async def work(ctx):
     await open_account(user)
     users = await get_bank_data()
     # print("USER STUFF", ctx, user.nick)
-    earnings = random.randrange(1001)
+    earnings = random.randrange(101)
     
-    await ctx.channel.send(f"{user} has earned {earnings} coins!")
+    await ctx.channel.send(f"{user.mention} has earned {earnings} coins!")
     
     users[str(user.id)]["wallet"] += earnings
     
@@ -79,17 +79,18 @@ async def coinflip(ctx, *arg):
     user = ctx.author
     users = await get_bank_data()
     wallet_amount = users[str(user.id)]["wallet"]
-    print(arg,"ARGUMENT")
     if arg == (): 
         await ctx.send("Provide a wager amount in numbers")
         return
     if arg[0] == "max":
         arg = int(wallet_amount), #comma turns assigned into tuple
+    elif arg[0] == "half":
+        arg = int(wallet_amount)/2, #comma turns assigned into tuple
     elif int(arg[0]) < 0:
-        await ctx.send("Nice try, loser")
+        await ctx.send(f"Nice try, {user.mention} you loser")
         return
     elif int(wallet_amount) < int(arg[0]):
-        await ctx.send("You're broke")
+        await ctx.send(f"You're broke, {user.mention}")
         return
     # elif int(arg[0]) > 10000:
     #     await ctx.send("You can only bet 10000 coins at a time")
@@ -131,52 +132,108 @@ async def on_message(message):
     # print("test", message)
     users = await get_bank_data()
 
-    random_multiplier = random.randint(1,10)
+    random_multiplier = random.randint(1,3)
     msg_len = len(message.content)
+    if msg_len > 200:
+        msg_len = 200
     earnings = random_multiplier * msg_len
     
     users[str(message.author.id)]["wallet"] += earnings
     with open("eco.json", "w") as f:
         json.dump(users, f)
+    return
 
 @bot.command()
 async def leaderboard(ctx, x=10):
-  with open('eco.json', 'r') as f:
+    with open('eco.json', 'r') as f:
     
-    users = json.load(f)
+        users = json.load(f)
     
-  leaderboard = {}
-  total=[]
+    leaderboard = {}
+    total=[]
   
-  for user in list(users):
-    name = int(user)
-    total_amt = users[str(user)]['wallet']
-    leaderboard[total_amt] = name
-    total.append(total_amt)
+    for user in list(users):
+        if user == "jackpot":
+           continue
+        name = int(user)
+        total_amt = users[str(user)]['wallet']
+        leaderboard[total_amt] = name
+        total.append(total_amt)
     
 
-  total = sorted(total,reverse=True)
+    total = sorted(total,reverse=True)
   
 
-  em = discord.Embed(
-    title = f'Top {x} richest members in {ctx.guild.name}',
-    description = 'The richest people in this server',
-    color=discord.Color.orange()
-  )
+    em = discord.Embed(
+        title = f'Top {x} richest members in {ctx.guild.name}',
+        description = 'The richest people in this server',
+        color=discord.Color.orange()
+    )
   
-  index = 1
-  for amt in total:
-    id_ = leaderboard[amt]
-    member = await bot.fetch_user(id_)  
-    em.add_field(name = f'{index}: {member}', value = f'{amt} coins', inline=False)
-    print(member)
+    index = 1
+    for amt in total:
+        id_ = leaderboard[amt]
+        member = await bot.fetch_user(id_)  
+        em.add_field(name = f'{index}: {member}', value = f'{amt} coins', inline=False)
+        print(member)
     
-    if index == x:
-      break
-    else:
-      index += 1
+        if index == x:
+            break
+        else:
+            index += 1
       
-  await ctx.send(embed = em)
+    await ctx.send(embed = em)
   
+  
+@bot.command()
+async def lotto(ctx):
+    user = ctx.author
+    users = await get_bank_data()
+    wallet_amount = users[str(user.id)]["wallet"]
+    if wallet_amount < 100:
+        await ctx.send(f"A lotto ticket costs 100 coins. \nYou only have {wallet_amount} coins, {user.mention}!")
+        return
+    # users["jackpot"] = 1000000
+    jackpot = users["jackpot"]
+    winning_num = random.randint(1,1001)
+    user_drawing = random.randint(1,1001)
+    with open("eco.json", "w") as f:
+        json.dump(users, f)
+    em = discord.Embed(
+        title = f"{user} drew {user_drawing}. The winning number was {winning_num}!",
+        color=discord.Color.teal()
+    )
+    if winning_num == user_drawing:
+        users[str(user.id)]["wallet"] += jackpot
+        jackpot = 1000000
+        with open("eco.json", "w") as f:
+            json.dump(users, f)
+        await get_bank_data()
+        new_balance = users[str(user.id)]["wallet"]
+        em.add_field(name = f"You have won the {jackpot} coin jackpot!", value = f"Your new balance is {new_balance}", inline=False)
+    else:
+        jackpot += 50
+        users["jackpot"] += 50
+        users[str(user.id)]["wallet"] -= 100
+        with open("eco.json", "w") as f:
+            json.dump(users, f)
+        await get_bank_data()
+        new_balance = users[str(user.id)]["wallet"]
+        em.add_field(name = f"You paid 100 coins for a ticket.\nYour ticket doesn't match the winning number.. \nSorry, better luck next time! \nYou now have {new_balance} coins.", value = f"The jackpot is now {jackpot}", inline=False)
+    
+    await ctx.send(embed = em)
+    
+@bot.command()
+async def jackpot(ctx):
+    user = ctx.author
+    users = await get_bank_data()
+    jackpot = users["jackpot"]
+    wallet = users[str(user.id)]["wallet"]
+    print(jackpot)
+    em = discord.Embed(
+        title = f"The jackpot is currently {jackpot} coins!"
+    )
+    em.add_field(name = f"Feelin' lucky?", value= f"You currently have {wallet} coins. \nIt costs 100 coins to play. \nType -lotto to try your luck!")
+    await ctx.send(embed = em)
 
 bot.run(token)
